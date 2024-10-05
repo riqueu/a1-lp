d903e333fe4c05056be537799a866fa0a1d82d3b
Henrique Coelho <riquecbeltrao@hotmail.com>
2024-10-05 15:22:06 -0300

    modularizando

---
+++ main.py
@@ -0,0 +1,25 @@
+from src import medalist_x_urbanization_analysis as mu
+from src import data_cleaner as dc
+import pandas as pd
+
+try:
+    # Criação dos DataFrames para Análise
+    athletes_df = pd.read_csv('data/athlete_events.csv')
+    noc_df = pd.read_csv('data/noc_regions.csv')
+    urbanization_df = pd.read_csv('data/urbanization.csv')
+    gdp_df = pd.read_csv('data/gdp/gdp.csv')
+    gdp_per_capita_df = pd.read_csv('data/gdp/gdp_per_capita.csv')
+
+    # Limpeza Inicial dos DataFrames
+    athletes_df = dc.medals_to_int(athletes_df)
+    athletes_df = dc.predict_missing(athletes_df)
+    urbanization_df.columns = ['Year', 'Economy_Code', 'Country', 'Pop_Absolute', 'Pop_Missing', 'Urban_Pop_Percent', 'Urban_Pop_Percent_Missing']
+    urbanization_df = urbanization_df[['Year', 'Country', 'Pop_Absolute', 'Urban_Pop_Percent']]
+    urbanization_df = dc.rename_countries(urbanization_df) # Renomear países para padrão do DataFrame de Atletas
+
+    # Análise de Densidade de Medalhas por População Urbana em 2016
+    data_2016 = mu.prepare_2016_medalist_urbanization_analysis(athletes_df, urbanization_df, noc_df)
+    mu.save_scatterplot_2016_medalist_urbanization(data_2016)
+
+except FileNotFoundError:
+    print("File not found, check if the path is correct.")
