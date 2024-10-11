5872e98ddfc078e4e36cf6386c7f8a9318a1a224
G4me0ver23 <carlosdaniellima5002@gmail.com>
2024-10-11 10:44:41 -0300

    Plot dos graficos de dispersao das olimpiadas

--- src/womens_participation_graphs.py
+++ src/womens_participation_graphs.py
@@ -8,19 +8,27 @@ from womens_participation import *
 
 df_olymp, df_olymp_countries, df_paralymp, df_paralymp_countries, df2_olymp, df2_olymp_bra, df2_paralymp, df2_paralymp_bra = create_dataframes()
 
-df_analisys1 = df_olymp
-df_analisys1 = df_analisys1[(df_analisys1['M_Score'] > 0) | (df_analisys1['F_Score'] > 0)]
-
-def plot_scatter_graph(df, x, y1, y2):
+def plot_scatter_graph(df, x, y1, y2, title, score_or_amount):
     # Criando o gráfico de dispersão
-    sns.scatterplot(x=x, y=y1, data=df, label=y1, color='purple', s=50, alpha=0.7)
-    sns.scatterplot(x=x, y=y2, data=df, label=y2, color='blue', s=50, alpha=0.7)
+    plt.figure(figsize=(12, 6))
+    sns.scatterplot(x=x, y=y1, data=df, label='Women', color='purple', s=70)
+    sns.scatterplot(x=x, y=y2, data=df, label='Men', color='blue', s=70)
 
     # Ajustando o gráfico
-    plt.xlabel('Ano')
-    plt.ylabel('Quantidade')
-    plt.title('Gráfico de Dispersão: Pontuação por Ano')
-    plt.legend(title='Grupos')
+    plt.xlabel('Year')
+    plt.ylabel(score_or_amount)
+    plt.title(title)
+    plt.legend(loc='upper left')
     plt.show()
     
-plot_scatter_graph(df_analisys1, 'Year', 'F_Medal', 'M_Medal')
+    
+df_analisys_aux = df_olymp
+#plot_scatter_graph(df_analisys_aux, 'Year', 'F_Athletes', 'M_Athletes', 'Scatter Plot: Male Athletes X Female Athletes (Global)', 'Amount')
+df_analisys_aux = df_analisys_aux[(df_analisys_aux['M_Score'] > 0) | (df_analisys_aux['F_Score'] > 0)]
+#plot_scatter_graph(df_analisys_aux, 'Year', 'F_Medal', 'M_Medal', 'Scatter Plot: Men\'s Score X Women\'s Score (Global)', 'Score')
+
+df_analisys_aux = df_olymp_countries[df_olymp_countries['NOC']=='BRA']
+#lot_scatter_graph(df_analisys_aux, 'Year', 'F_Athletes', 'M_Athletes', 'Scatter Plot: Male Athletes X Female Athletes (Brazil)', 'Amount')
+df_analisys_aux = df_analisys_aux[(df_analisys_aux['M_Score'] > 0) | (df_analisys_aux['F_Score'] > 0)]
+#plot_scatter_graph(df_analisys_aux, 'Year', 'F_Medal', 'M_Medal', 'Scatter Plot: Men\'s Score X Women\'s Score (Brazil)', 'Score')
+
