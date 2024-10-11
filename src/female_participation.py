fa33e01da3bc711d94bb41c10efc3024104f48f9
G4me0ver23 <carlosdaniellima5002@gmail.com>
2024-10-11 02:31:58 -0300

    atualizando female_participation

--- src/female_participation.py
+++ src/female_participation.py
@@ -4,21 +4,125 @@ import seaborn as sns
 from data_cleaner import *
 
 
-df = pd.read_csv("data/athlete_events.csv")
-df = medals_to_int(df)
+def count_athletes(df, *args):
+    df = df.groupby(list(args))['Medal'].count().unstack(fill_value=0)
+    df['Total_Athletes'] = df.sum(axis=1)
+    df = df.reset_index()
+    df.rename(columns={'F': 'F_Athletes', 'M': 'M_Athletes'}, inplace=True)
+    return df
 
-# Filtra o dataframe para pegar apenas os atletas do Brasil
-bra_df = df[df['NOC']=='BRA']
+def update_medals_or_score(df, medal_or_score, *args, **kwargs):
+    if medal_or_score == 'Medal':
+        df = df.groupby(list(args))['Medal'].apply(lambda x:  (x != 0).sum()).unstack(fill_value=0).reset_index()
+    else:
+        df = df.groupby(list(args))['Medal'].apply(lambda x: x.sum()).unstack(fill_value=0).reset_index()
+        
+    df.rename(columns=kwargs, inplace=True)
+    df[f'Total_{medal_or_score}'] = df[[f'F_{medal_or_score}', f'M_{medal_or_score}']].sum(axis=1)
+    
+    
+    return df
 
-# Primeira analise: Quantidade de medalhas gerais dos atletas de todos os paises por ano e Quantidade de medalhas de atletas femininas de todos os paises por ano
-# Soma a quantidade de medalhas de atletas de todos os paises agrupada por ano
-medals_year = df.groupby(['Year', 'Sex'])['Medal'].apply(lambda x: (x != 0).sum()).unstack(fill_value=0)
-medals_year['Total'] = medals_year.sum(axis=1)
-print(medals_year.head())
+df1 = pd.read_csv("data/athlete_events.csv")
+df2 = pd.read_csv("data/modified_medal_athlete.csv")
+df3 = pd.read_csv("data/summer_paralympics.csv")
+df4 = pd.read_csv("data/winter_paralympics.csv")
+df3 = pd.concat([df3, df4])
+df3.sort_values(by=['Year'], inplace=True)
+df1 = medals_to_int(df1)
 
 
-# Soma a quantidade de medalhas de atletas do brasil agrupada por ano
-medals_bra_year = bra_df.groupby(['Year', 'Sex'])['Medal'].apply(lambda x: (x != 0).sum()).unstack(fill_value=0)
-medals_bra_year['Total'] = medals_bra_year.sum(axis=1)
-print(medals_bra_year.head())
+# Analise 1: Participacao dos atletas do brasil (df1, df3 e df4) comparado com o mundo (agrupado por ano)
+# Separado em paises por ano
+df_olymp_countries = df1.drop_duplicates(['Year', 'Name'])
+df_olymp_countries = count_athletes(df_olymp_countries, *['Year', 'NOC','Sex'])
 
+df_paralymp_countries = df3[['Year', 'Country_Code', 'Women', 'Men', 'P_Total']]
+df_paralymp_countries = df_paralymp_countries.rename(columns={'Country_Code': 'NOC', 'Women': 'F_Athletes', 'Men': 'M_Athletes', 'P_Total': 'Total_Athletes'})
+
+# Todos os paises agrupados por ano
+df_olymp = df1.drop_duplicates(['Year', 'Name'])
+df_olymp = count_athletes(df_olymp, *['Year', 'Sex']) 
+
+df_paralymp = df3.groupby(['Year'])[['Men', 'Women', 'P_Total']].sum()
+df_paralymp.rename(columns={'Women': 'F_Athletes', 'Men': 'M_Athletes', 'P_Total': 'Total_Athletes'}, inplace=True)
+
+# Conta a quantidade de medalhas separada pelo genero do atleta e agrupada por ano
+# Quantidade de medalhas por país por ano
+df_aux = update_medals_or_score(df1, 'Medal', *['Year', 'NOC', 'Sex'], **{'F': 'F_Medal', 'M': 'M_Medal'})
+df_olymp_countries = pd.merge(
+        df_olymp_countries,
+        df_aux,
+        how='outer',
+        on=['Year', 'NOC']
+    )
+
+df_aux = update_medals_or_score(df2, 'Medal', *['Games_year', 'Npc_new', 'Sex'], **{'Games_year': 'Year','Npc_new': 'NOC', 'F': 'F_Medal', 'M': 'M_Medal'})
+
+df_paralymp_countries = pd.merge(
+        df_paralymp_countries,
+        df_aux,
+        how='outer',
+        on=['Year', 'NOC']
+    )
+
+df_paralymp_countries.dropna(inplace=True)
+
+df_aux = update_medals_or_score(df1, 'Medal', *['Year', 'Sex'], **{'F': 'F_Medal', 'M': 'M_Medal'})
+df_olymp = pd.merge(
+        df_olymp,
+        df_aux,
+        how='outer',
+        on='Year'
+    )
+
+
+df_aux = update_medals_or_score(df2, 'Medal', *['Games_year', 'Sex'], **{'Games_year': 'Year', 'F': 'F_Medal', 'M': 'M_Medal'})
+df_paralymp = pd.merge(
+        df_paralymp,
+        df_aux,
+        how='outer',
+        on='Year'
+    )
+
+df_paralymp.dropna(inplace=True)
+
+# Pontuacao do pais por ano (baseado no peso das medalhas)
+df_aux = update_medals_or_score(df1, 'Score', *['Year', 'NOC', 'Sex'], **{'F': 'F_Score', 'M': 'M_Score'})
+df_olymp_countries = pd.merge(
+        df_olymp_countries,
+        df_aux,
+        how='outer',
+        on=['Year', 'NOC']
+    )
+
+df_aux = update_medals_or_score(df2, 'Score', *['Games_year', 'Npc_new', 'Sex'], **{'Games_year': 'Year','Npc_new': 'NOC', 'F': 'F_Score', 'M': 'M_Score'})
+
+df_paralymp_countries = pd.merge(
+        df_paralymp_countries,
+        df_aux,
+        how='outer',
+        on=['Year', 'NOC']
+    )
+df_paralymp_countries.dropna(inplace=True)
+
+df_paralymp_countries[df_paralymp_countries.columns.difference(['NOC'])] = df_paralymp_countries[df_paralymp_countries.columns.difference(['NOC'])].astype(int)
+
+df_aux = update_medals_or_score(df1, 'Score', *['Year', 'Sex'], **{'F': 'F_Score', 'M': 'M_Score'})
+df_olymp = pd.merge(
+        df_olymp,
+        df_aux,
+        how='outer',
+        on='Year'
+    )
+
+df_aux = update_medals_or_score(df2, 'Score', *['Games_year', 'Sex'], **{'Games_year': 'Year', 'F': 'F_Score', 'M': 'M_Score'})
+df_paralymp = pd.merge(
+        df_paralymp,
+        df_aux,
+        how='outer',
+        on='Year'
+    )
+
+df_paralymp.dropna(inplace=True)
+df_paralymp = df_paralymp.astype('int64')
