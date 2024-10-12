6860c98ca016b0c4cf4a2857a7518f88439564e2
Filipe Novaes <lfns3@aluno.ifal.edu.br>
2024-10-12 03:53:09 -0300

    feat: improve the graphs

--- src/olympics_paralympics_pib_analysis.py
+++ src/olympics_paralympics_pib_analysis.py
@@ -131,7 +131,7 @@ def prepare_data_for_analysis(athletes_df: pd.DataFrame, summer_paralympics_df: 
 
         combined_df = fill_nan_gdp_with_interpolation(combined_df)
 
-        # Converter o PIB de unidades originais para bilhões
+        # Converte o PIB para bilhões
         combined_df['GDP'] = combined_df['GDP'] / 1e9
 
         return combined_df.sort_values(by=['Year', 'Country', 'Event'])
@@ -139,6 +139,36 @@ def prepare_data_for_analysis(athletes_df: pd.DataFrame, summer_paralympics_df: 
         raise KeyError(f"KeyError: Missing one or more required columns in the dataframes:")
     except Exception as error:
         raise Exception(f"Error preparing data for analysis: {str(error)}")
+
+
+def prepare_total_medals_gdp_analysis(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Prepara os dados para análise de correlação entre medalhas e PIB.
+
+    Args:
+        data (pd.DataFrame): DataFrame contendo colunas 'Gold', 'Silver', 'Bronze' e 'GDP'.
+
+    Returns:
+        pd.DataFrame: Matriz de correlação entre as medalhas de ouro, prata, bronze e o PIB.
+    """
+    correlation_matrix = data[['M_Total', 'GDP']].corr()
+
+    return correlation_matrix
+
+
+def prepare_medals_categories_gdp_analysis(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Prepara os dados para análise de correlação entre medalhas e PIB.
+
+    Args:
+        data (pd.DataFrame): DataFrame contendo colunas 'Gold', 'Silver', 'Bronze' e 'GDP'.
+
+    Returns:
+        pd.DataFrame: Matriz de correlação entre as medalhas de ouro, prata, bronze e o PIB.
+    """
+    correlation_matrix = data[['Gold', 'Silver', 'Bronze', 'GDP']].corr()
+
+    return correlation_matrix
 
 
 def prepare_olympics_paralympics_analysis(data: pd.DataFrame) -> pd.DataFrame:
@@ -152,31 +182,14 @@ def prepare_olympics_paralympics_analysis(data: pd.DataFrame) -> pd.DataFrame:
         pd.DataFrame: Matriz de correlação entre as medalhas de ouro, bronze e prata das Olimpíadas e Paralimpíadas.
     """
     grouped_data = data.groupby(['Country', 'Event']).agg({
-        'Gold': 'sum',
-        'Silver': 'sum',
-        'Bronze': 'sum'
+        'M_Total': 'sum'
     }).reset_index()
 
-    # Pivotar o DataFrame para ter eventos como colunas
-    heatmap_data = grouped_data.pivot_table(index='Country', columns='Event', values=['Gold', 'Silver', 'Bronze'], fill_value=0)
+    # Pivota o DataFrame para ter eventos como colunas
+    heatmap_data = grouped_data.pivot_table(index='Country', columns='Event', values=['M_Total'], fill_value=0)
 
-    # Calcular a correlação entre as medalhas de ouro, prata e bronze
+    # Calcula a correlação entre as medalhas de ouro, prata e bronze
     correlation_matrix = heatmap_data.corr()
-
-    return correlation_matrix
-
-
-def prepare_medals_gdp_analysis(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Prepara os dados para análise de correlação entre medalhas e PIB.
-
-    Args:
-        data (pd.DataFrame): DataFrame contendo colunas 'Gold', 'Silver', 'Bronze' e 'GDP'.
-
-    Returns:
-        pd.DataFrame: Matriz de correlação entre as medalhas de ouro, prata, bronze e o PIB.
-    """
-    correlation_matrix = data[['Gold', 'Silver', 'Bronze', 'GDP']].corr()
 
     return correlation_matrix
 
@@ -194,20 +207,20 @@ def prepare_2016_olympics_paralympics_pib_analysis(data: pd.DataFrame) -> pd.Dat
         pd.DataFrame: DataFrame preparado com as colunas 'Country', 'M_Olympics', 
             'M_Paralympics' e 'GDP' para o ano de 2016.
     """
-    # Filtrar os dados para incluir apenas o ano de 2016 e países com mais de 5 medalhas
+    # Filtra os dados para incluir apenas o ano de 2016 e países com mais de 5 medalhas
     data_2016 = data[(data['Year'] == 2016) & (data['M_Total'] > 5)]
     
-    # Agrupar os dados por país e evento (Olimpíadas e Paralimpíadas)
+    # Agrupa os dados por país e evento (Olimpíadas e Paralimpíadas)
     grouped_data = data_2016.groupby(['Country', 'Event']).agg({
         'M_Total': 'sum',
         'GDP': 'mean'
     }).reset_index()
 
-    # Criar colunas separadas para Olimpíadas e Paralimpíadas
+    # Cria colunas separadas para Olimpíadas e Paralimpíadas
     olympics_data = grouped_data[grouped_data['Event'] == 'Olympics'].rename(columns={'M_Total': 'M_Olympics'})
     paralympics_data = grouped_data[grouped_data['Event'] == 'Paralympics'].rename(columns={'M_Total': 'M_Paralympics'})
 
-    # Unir os dados de Olimpíadas e Paralimpíadas
+    # Une os dados de Olimpíadas e Paralimpíadas
     merged_data = pd.merge(olympics_data[['Country', 'M_Olympics', 'GDP']], paralympics_data[['Country', 'M_Paralympics']], 
                            on='Country', how='outer').fillna(0)
 
@@ -232,7 +245,7 @@ def create_heatmap(correlation_matrix: pd.DataFrame, title: str) -> None:
     return plt
 
 
-def create_scatterplot_olympics_paralympics_pib_2016(data_2016: pd.DataFrame) -> None:
+def create_scatterplot_olympics_paralympics_pib_2016(data_2016: pd.DataFrame, xlim: tuple = None, ylim: tuple = None, zlim: tuple = None) -> None:
     """
     Cria um gráfico de dispersão 3D que mostra a relação entre medalhas nas
     Olimpíadas, nas Paralimpíadas e PIB dos países em 2016.
@@ -259,5 +272,12 @@ def create_scatterplot_olympics_paralympics_pib_2016(data_2016: pd.DataFrame) ->
     ax.set_title('3D Dispersion: Medals in the Olympics and Paralympics vs GDP (2016)')
 
     ax.view_init(elev=20, azim=-70)
+
+    if xlim:
+        ax.set_xlim(xlim)
+    if ylim:
+        ax.set_ylim(ylim)
+    if zlim:
+        ax.set_zlim(zlim)
 
     return plt
